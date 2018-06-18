617acc69ec337df22cd7ef0e965670e2f2b7a8d1
Tony Rasa <trasa@meancat.com>
2018-06-18 15:33:16 -0700

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -4,4 +4,4 @@ docker run -d \
     -p 8080:8080 \
     --name dev-dummy-game-service \
     -e "SERVICE_TAGS=dev" \
-    registry.wargaming.net/freya/dummy-game-service
+    yourregistryhere.com/yourprojecthere/dummy-game-service
